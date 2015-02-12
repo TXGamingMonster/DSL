4be4c5414be40202b6f4148621c3b84361e4c9a1
Jack Wesley Nelson <janelson@mymail.mines.edu>
2015-02-12 12:38:47 -0700

    several working tests, need to write more to ensure valid file loads, etc.

--- processor_tests.rb
+++ processor_tests.rb
@@ -3,9 +3,22 @@ require "test/unit"
 
 class TestProcessor < Test::Unit::TestCase
 
-	#check that Processor responds to methods in business file
+	#check that Processor responds to methods
 	def test_response
-		assert_respond_to(Processor, :product)
+		assert_respond_to(Processor.instance, :getProducts)
+		assert_respond_to(Processor.instance, :add_product)
+		assert_respond_to(Processor.instance, :last_product)
+		assert_respond_to(Processor.instance, :print)
+		assert_respond_to(Processor.instance, :loadRules)
 	end
+	def testFiles
+		#check that bad file throws error
+		assert_raise NoMethodError do
+			Processor.instance.load "businessRules.txt"
+		end
+		#check that valid file throws no error
+		
+	end
+	
 	
 end
